7adf0bfbc88fb57a021804907a3cacf4bcdec5b1
Vincent Chang <vincentchang402@gmail.com>
2024-02-08 09:34:30 +0800

    Add padTwo function to FormatTime utility

--- src/utils/FormatTime.js
+++ src/utils/FormatTime.js
@@ -1,8 +1,11 @@
 export const MS_IN_SECOND = 1000
 
+function padTwo(number) {
+  return number >= 10 ? String(number) : `0${number}`
+}
 export function formatTime(time) {
   return {
     seconds: Math.floor(time / MS_IN_SECOND),
-    tenMs: Math.floor((time % MS_IN_SECOND) / 10),
+    tenMs: padTwo(Math.floor((time % MS_IN_SECOND) / 10)),
   }
 }
