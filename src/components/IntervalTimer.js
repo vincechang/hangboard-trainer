c230befa001798be639d452b4ba3c3117ffe300d
Vincent Chang <vincentchang402@gmail.com>
2024-02-08 10:45:11 +0800

    Implement the fundamental repeaters feature

--- src/components/IntervalTimer.js
+++ src/components/IntervalTimer.js
@@ -1,5 +1,7 @@
-import {useState} from 'react'
-import {formatTime} from '../utils/FormatTime'
+import {useRef, useState} from 'react'
+import {formatTime, MS_IN_SECOND} from '../utils/FormatTime'
+import {useInterval} from '../hooks/UseInterval'
+
 const HANG = 'HANG'
 const OFF = 'OFF'
 const REST = 'REST'
@@ -9,15 +11,42 @@ const OFF_DURATION = 3
 const REST_DURATION = 60
 
 function IntervalTimer({sequence}) {
-  const [duration] = useState(6999)
+  const lastTick = useRef(null)
+  const [duration, setDuration] = useState(6999)
   const [isRunning, setIsRunning] = useState(false)
+  const [state, setState] = useState(HANG)
 
   const formattedTime = formatTime(duration)
+
+  useInterval(
+    () => {
+      const now = Date.now()
+      const timePassed = now - lastTick.current
+      setDuration(duration => {
+        let newDuration = duration - timePassed
+        if (newDuration < 0) {
+          if (state === HANG) {
+            newDuration += OFF_DURATION * MS_IN_SECOND
+            setState(OFF)
+          } else if (state === OFF) {
+            newDuration += HANG_DURATION * MS_IN_SECOND
+            setState(HANG)
+          } else if (state === REST) {
+            newDuration += HANG_DURATION * MS_IN_SECOND
+          }
+        }
+        return newDuration
+      })
+      lastTick.current = Date.now()
+    },
+    isRunning ? 1 : null,
+  )
 
   function toggleTimer() {
     if (isRunning) {
       setIsRunning(false)
     } else {
+      lastTick.current = Date.now()
       setIsRunning(true)
     }
   }
