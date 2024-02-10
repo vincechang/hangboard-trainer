45adca4a886ac3800047b63cec8840ee7c8363d1
Vincent Chang <vincentchang402@gmail.com>
2024-02-11 01:00:45 +0800

    Add the Nord color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nord00: '#2e3440',
+        nord01: '#3b4252',
+        nord02: '#434c5e',
+        nord03: '#4c566a',
+        nord04: '#d8dee9',
+        nord05: '#e5e9f0',
+        norc06: '#eceff4',
+        nord07: '#8fbcbb',
+        nord08: '#88c0d0',
+        nord09: '#81a1c1',
+        nord10: '#5e81ac',
+        nord11: '#bf616a',
+        nord12: '#d08770',
+        nord13: '#ebcb8b',
+        nord14: '#a3be8c',
+        nord15: '#b48ead',
+      },
+    },
   },
   plugins: [],
 }
